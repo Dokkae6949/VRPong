ae78b3cf84fbff9d6e910ba88a60902d136cf024
Dokkae <90514461+Dokkae6949@users.noreply.github.com>
2023-05-09 14:46:45 +0200

    Added process type selector to BounceBody3D

--- addons/BounceBody3D/bounce_body_3d.gd
+++ addons/BounceBody3D/bounce_body_3d.gd
@@ -4,12 +4,30 @@ class_name BounceBody3D
 
 signal body_collided(body: Node)
 
+enum ProcessingType {PHYSICS, NORMAL}
 
 @export var bounce_enabled := true
 @export var velocity: Vector3
+@export var processing_type: ProcessingType : set = set_processing_type
 
+
+func set_processing_type(type: ProcessingType) -> void:
+	if type == ProcessingType.NORMAL:
+		set_process(true)
+		set_physics_process(false)
+	else:
+		set_process(false)
+		set_physics_process(true)
+
+
+func _process(delta: float) -> void:
+	_update(delta)
 
 func _physics_process(delta: float) -> void:
+	_update(delta)
+
+
+func _update(delta: float) -> void:
 	var collision_info = move_and_collide(velocity * delta)
 	
 	if collision_info and bounce_enabled:
